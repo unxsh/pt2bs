fa4a9d2483e893f84b9dbdb951846c24d880d9ff
sfome <semenilichevsky@gmail.com>
2022-07-01 14:29:34 +0300

    Update: prepared the architecture of the project

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="pt2bs",
+    description="Python to binary build system based on nuitka.",
+    version="0.1",
+    license="MIT",
+    author="ssleert",
+    author_email="ssleert@gmail.com",
+    url="https://github.com/unxsh/pt2bs",
+    packages=find_packages("src"),
+    install_requires=["nuitka"],
+    keywords=["python", "nuitka", "pbs", "compiler", "exe", "bin", "build"],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows"
+    ],
+    entry_points={
+        'console_scripts': [
+            'pt2bs = pt2bs.pt2bs:main'
+        ]
+    }
+)
